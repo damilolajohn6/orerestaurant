8b26d4e6fbffd355d1867cf1d139b559dba62bf0
Damilola john <damilolajohn622@gmail.com>
2024-08-01 01:33:15 +0100

    fixing deployment bug

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from app import create_app
-from flask import Flask
 
-
-app = create_app()
+# Create an application instance
+app = create_app(config_name='production')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the app with debug mode off for production
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
