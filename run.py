6b0cc305db3e67e2fcf0a99f9a2b03a7aa760f30
Damilola john <damilolajohn622@gmail.com>
2024-08-01 05:11:08 +0100

    fix os issue

--- run.py
+++ run.py
@@ -8,3 +8,4 @@ app = create_app(config_name='production')
 if __name__ == '__main__':
     # Run the app with debug mode off for production
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
